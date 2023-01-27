9b0077f4f42f7178751c8d9c2f4b9c390da00b7d
Vasiliy <Wasyok.ru@yandex.ru>
2023-01-27 09:44:54 +0500

    1

--- todo_list/src/App.tsx
+++ todo_list/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Card from "./Components/Card/card";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 };
 
 export default App;
---
+++ todo_list/src/Components/Card/card.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+
+const Card: FC = () => {
+  return (
+    <div>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          border: "3px solid teal",
+          background: "blue",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Card;
