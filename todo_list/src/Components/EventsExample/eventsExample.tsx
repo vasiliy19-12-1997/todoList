de9b2fcb00808d7924093f5d0b01e31c3277f805
Vasiliy <Wasyok.ru@yandex.ru>
2023-01-28 11:31:32 +0500

    events and ref

--- todo_list/src/Components/EventsExample/eventsExample.tsx
+++ todo_list/src/Components/EventsExample/eventsExample.tsx
@@ -1,23 +1,54 @@
-import React, { FC, ReactNode, useState } from "react";
+import React, { FC, ReactNode, useRef, useState } from "react";
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>("");
+  const [isDrag, setIsDrag] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log("Drag");
+  };
+  const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+    console.log("Drop");
+  };
+  const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(true);
+  };
+  const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(true);
+  };
+
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(value);
+    console.log(inputRef.current?.value);
   };
   return (
     <div>
+      <input ref={inputRef} />
       <input onChange={changeHandler} />
       <button onClick={clickHandler}>fdfdfdf</button>
-      <div style={{ width: "200px", height: "200px", background: "red" }}></div>
       <div
+        onDrop={dragHandler}
+        draggable
         style={{
           width: "200px",
           height: "200px",
-          background: "blue",
+          background: isDrag ? "blue" : "red",
+        }}
+      ></div>
+      <div
+        onDrop={dropHandler}
+        onDragLeave={leaveHandler}
+        onDragOver={dragWithPreventHandler}
+        style={{
+          width: "200px",
+          height: "200px",
+          background: "red",
           marginTop: "20px",
         }}
       ></div>
